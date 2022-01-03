0a4df3c42eecd8fc14a51ba2789f0851eb5cf910
AMIT KUMAR <ak4302392@gmail.com>
2022-01-03 16:44:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import styled from "styled-components"
+
+const styledMainContainer=styled.div`
+font-size: 1.5em;
+text-align: center;
+color: palevioletred;
+
+
+`;
+function App() {
+  return (
+    
+    <styledMainContainer>
+      Hello World!
+    </styledMainContainer>
+    
+  );
+}
+
+export default App;
